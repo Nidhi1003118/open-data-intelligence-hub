62a364f8c72a9c1ad3056d6180602f5822e7f62b
Gayathri-249 <gayathrisobanala@gmail.com>
2026-06-26 16:17:58 +0530

    Added Task 3 EDA notebook

--- open-data-intelligence-hub/Mentee Contribution/Task 1 - Data Analysis using Polars/Gayathri/src/cleaner.py
+++
@@ -1,6 +0,0 @@
-import polars as pl
-
-df = pl.read_csv("data1/world_happiness.csv")
-print(df.null_count())
-df = df.drop_nulls()
-print(df.shape)
--- open-data-intelligence-hub/Mentee Contribution/Task 1 - Data Analysis using Polars/Gayathri/src/loader.py
+++
@@ -1,7 +0,0 @@
-import polars as pl
-
-df = pl.read_csv(
-    "data1/world_happiness.csv"
-)
-
-print(df.head())
